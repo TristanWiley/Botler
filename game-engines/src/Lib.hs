aa0bc5895d2f94d0f2a2fad2e5dbd6f5fc332bc2
Ellie <ellie.seoe.jung@gmail.com>
2015-10-24 19:02:19 -0400

    Revised tic tac toe game.

--- game-engines/src/Lib.hs
+++ game-engines/src/Lib.hs
@@ -14,11 +14,12 @@ import Data.Aeson
 import Data.Aeson.Lens
 import Data.Char
 import Data.Conduit
+import Data.List(intercalate)
 import Data.Maybe
 import Data.Monoid
 import System.IO
-import qualified Data.Array as A
 import qualified Data.Aeson.TH as AT
+import qualified Data.Array as A
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.Conduit.Binary as CB
@@ -149,11 +150,110 @@ tronBikeGame size@(width, height) = Game {
     }
 
 -- 2-player Tic-Tac-Toe
-data TTTMove = A0 | A1 | A2|
-               B0 | B1 | B2|
-               C0 | C1 | C2 deriving (Eq, Show)
+data TTTMove = O | X deriving (Eq, Show, Enum, Ord)
+type Position = (Char, Int)
+data BoardMove = BoardMove
+                { bMove :: Maybe Move, bPos :: Position } 
+                deriving (Eq, Show)
+type Board = [BoardMove]
+type InvalidMove = String
 
-{-data TTTState = A0taken | A1taken | A2taken |
+bsize = 3
+coord = [('A'..),('1',..)]
+
+empty :: Int -> Board
+empty size = do
+    x <- take size (fst coord)
+    y <- take size (snd coord)
+return boardMove Nothing(x,y)
+
+move :: BoardMove -> Board -> Either InvalidMove Board
+move (BoardMove _ (c,r))[]=
+    Left $ "Invalid move" ++ [c] ++ (show r)
+move bm@(BoardMove nmov npos)(x:xs)
+    | findMove x = Right $ bm:xs
+    | otherwise =
+      case move bm xs of
+        Right r -> Right $ x:r
+        err     -> err
+      where findMove (BoardMove m p) =
+                p == npos && isNothing m && nmov /= Nothing
+
+win :: BoardMove -> Board -> Bool
+win (BoardMove Nothing _) _ = False
+win (BoardMove m (c,r)) b = row || col || diag' cb || diag' (reverse cb)
+    where row = length
+                (filter (\(BoardMove m2 (_,r2)) ->
+                        m2 == m && r2 == r) b) == bsize
+          col = length
+                (filter (\(BoardMove m2 (c2,_)) ->
+                        m2 == m && c2 == c) b) == bsize
+          diag' xss = all (\(BoardMove m2 _) ->
+                        m2 == m) $ diag xss
+          cb = chop bsize b
+
+draw :: BoardMove -> Board -> Bool
+draw bm b = not (any (isNothing . bMOve) b)
+          && not (win bm b)
+
+printBoard :: Board -> String
+printBoard b = intercalate "\n" $
+                map (\row-> [(fst. bPos) (row !! 0)] ++ ") |" ++
+                    (intercalate " | "
+                        $ map (\bm-> maybe " " show $ bMove bm) row)
+                    ++ " |")
+                (chop bsize b)
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+diag :: [[a]] -> [a]
+diag xss = [xss !! n !! n | n <- [0 .. length xss -1]]
+
+main = do
+    putStrLn "Starting new game. . ."
+    putStrLn "Type 'quit' to exit game"
+    let newBoard = empty bsize
+        in do (putStrLn .  (\s->"\n"++s++"\n") . printBoard) newBoard
+            gameloop Nothing newBoard
+
+gameloop prevMove board = do
+    let currPlayer = maybe  (\BoardMove mv _) ->
+                                case mv of
+                                    Just X -> O
+                                    Just O -> X) prevMOve
+    putSTr $ "Player '" ++ (show currPlayer) ++ "': "
+    hFlush stdout
+    playerMove <- getLine
+    case (playerMove, (map toUpper playerMove) 'elem' allCoord) of
+        ("quit", _) ->
+            putStrLn "K. BYE."
+        (_, False ) -> do
+            putStrLn $ "Possible options: " ++ intercalate ", " allCoord
+            gameloop prevMove board
+        otherwise   -> do
+            let pos = (toUpper $ playerMove !! 0,
+                       read [(playerove !! 1)] :: Int)
+                currMove = BoardMove (Just currPlayer) pos
+                currBoard = move currMove board
+            either putStrLn (putStrLn . (\s->"\n"++s++"\n") . printBoard) currBoard
+            case currBoard of
+                Right r -> if win currMove r
+                            then do putStrLn $ "Player '"
+                                                ++ (show currPlayer) ++ "' wins!"
+                                    main
+                            else if draw currMove r
+                                    then do putStrLn $ "No one wins, no one losses!"
+                                        main
+                            else gameloop (Just currMove) r
+                Left err -> gameloop prevMove board
+    where allCoord = [[s] ++ show y | x <- take bsize (fst coord),
+                                      y <- take bsize (snd coord)]
+
+
+
+{-data TTTState = A0taken | A1taken | A2taken|
                 B0taken | B1taken | B3taken |
                 C0taken | C1taken | C3taken deriving Show
 -}
