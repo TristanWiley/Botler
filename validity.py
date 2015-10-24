aef080c6eea97affe0321f13cd9b8e9dd56b7f87
Elliott <elliottweintraub@gmail.com>
2015-10-24 02:56:02 -0400

    "I have just done a validity testor in python using a json import to test a ruleset against a bot input

---
+++ validity.py
@@ -0,0 +1,9 @@
+import json
+def valid_input(ruleset, bot_response):
+   if bot_response in ruleset["good"]:
+       print "Good Response!"
+   else:
+       print "What was that response? Get out of here"
+ruleset = {"good":{'0', '1', '2', 'r', 'p', 's', 'rock', 'paper'}, "bad":{'3', '4', '0'}}
+bot_response = "1"
+valid_input(ruleset, bot_response)
