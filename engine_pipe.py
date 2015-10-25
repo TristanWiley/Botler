25cd3c1400f89027ca8e818cf0a3317e5aac4351
JEphron <jephron63@gmail.com>
2015-10-24 20:46:35 -0400

    Engine-comms working

--- engine_pipe.py
+++ engine_pipe.py
@@ -2,27 +2,40 @@
 import subprocess
 import sys
 from subprocess import PIPE, Popen
-from threading  import Thread
+from threading import Thread
 import json
 
+
 class Engine:
+
     def __init__(self, program):
         self.program = program
 
-        print 'One line at a time:'
-        self.proc = subprocess.Popen('game-engines/build_and_run.sh', 
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE)
+        self.proc = subprocess.Popen('game-engines/build_and_run.sh',
+                                     stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE)
 
-    def get_world_state(self):
+    def read(self):
         try:
             output = self.proc.stdout.readline()
         except ValueError:
             print sys.exc_info()
-        return json.decode(output.rstrip())
+        return json.loads(output.rstrip())
 
-
-    def update(self, data_string): # see spec?
-        # pipe.send(data_string) 
+    def write(self, data_string):  # see spec?
         self.proc.stdin.write('%s\n' % data_string)
 
+    def getState(self):
+        self.write('["getState"]')
+        return self.read()
+
+    def checkStatus(self):
+        self.write('["checkStatus"]')
+        return self.read()
+
+    def makeMove(self, move):
+        self.write('["makeMove", %s]' % json.dumps(move))
+        
+    def validMoves(self):
+        self.write('["validMoves"]')
+        return self.read()
