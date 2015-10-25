4740ab707c4cb1f94f918af5e8560d406616860d
JEphron <jephron63@gmail.com>
2015-10-25 09:38:42 -0400

    Fix process leaks

--- game-engines/build_and_run.sh
+++ game-engines/build_and_run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $(dirname "$0")
-stack build --exec "$(find . -name 'game-engines-exe' | grep bin) $@"
+stack build
+exec $(find . -name 'game-engines-exe' | grep bin) $@
--- player_scripts/rps-rando_stuffs.py
+++
@@ -1,7 +0,0 @@
-#My awesome bot that plays games
-
-from random import choice
-
-def main(data):
-    return choice(data['valid_moves'])
-                            
